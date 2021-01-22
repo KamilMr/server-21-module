ccb195cd27e5265fb9f96dedb9ed1eb2b7ff620e
Kamil Mrowka <kamil.mrowka@outlook.com.au>
2021-01-22 13:40:57 +0100

    add darkModeTheme

--- server.js
+++ server.js
@@ -1,43 +1,35 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
+
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', {layout: 'dark'});
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history');
 });
 
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/style.css'));
-});
-
-app.get('/test.png', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/test.png'));
-});
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
